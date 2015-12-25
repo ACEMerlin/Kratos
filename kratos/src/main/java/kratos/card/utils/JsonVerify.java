a0b3df14f1508576c439d871b59d87658c44f957
changan.luo <changan.luo@ele.me>
2015-12-25 11:34:31 +0800

    add jsonverify

---
+++ kratos/src/main/java/kratos/card/utils/JsonVerify.java
@@ -0,0 +1,29 @@
+package kratos.card.utils;
+
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+
+/**
+ * To verify whether a json is legal
+ * Created by Wayne on 15/12/25.
+ *
+ */
+public class JsonVerify {
+
+    public static boolean isIllegalJson(String json) {
+        return !isLegalJson(json);
+    }
+
+    public static boolean isLegalJson(String json) {
+        if ((json).isEmpty()) {
+            return false;
+        }
+        try {
+            new JsonParser().parse(json);
+            return true;
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
